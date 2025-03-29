ad11a6b565d0ed3eb4b53f9a435f9daf7056bcc6
Harshal Tribhuwan <harshaltribhuwan12345@gmail.com>
2025-03-29 17:23:13 +0530

    done with the chagnes for the line chart

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-} from "recharts";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 import { lineChartData } from "./mockData.js";
 import "../styles/Charts.scss";
 
@@ -14,12 +7,21 @@ const CustomLineChart = () => {
   return (
     <div className="chart-container line-chart">
       <h4>Completed Articles</h4>
-      <LineChart width={400} height={200} data={lineChartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
+      <LineChart
+        width={400}
+        height={200}
+        data={lineChartData}
+        margin={{ top: 10, right: 20, left: 20, bottom: 20 }}
+      >
+        <XAxis
+          dataKey="date"
+          tickLine={false}
+          interval={1}
+          padding={{ left: 10, right: 10 }}
+        />
+        <YAxis width={30} tickLine={false} axisLine={true} tick={{ dx: -5 }} />
         <Tooltip />
-        <Line type="monotone" dataKey="articles" stroke="#2196f3" />
+        <Line type="monotone" dataKey="articles" stroke="#0070B0" dot={false} />
       </LineChart>
     </div>
   );
